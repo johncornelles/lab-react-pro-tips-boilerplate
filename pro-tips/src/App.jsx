5c2ec07381aacc6a92d2420a2922e5cd24f17bfc
johncornelles <john.cornellews@kalvium.community>
2024-01-26 22:34:40 +0530

    Update the Navbar component and add a Form component.

--- pro-tips/src/App.jsx
+++ pro-tips/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter , Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Contacts from './components/Contacts';
 import Form from './components/Form/Form';
-import Navbar from './components/Navbar';
+import Navbar from './Navbar';
 import './App.css'
 
 
--- pro-tips/src/Navbar.jsx
+++ pro-tips/src/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
-import './../../App.css'
-
+import './App.css'
 const Navbar = () => {
   return (
     <nav className="navbar">
